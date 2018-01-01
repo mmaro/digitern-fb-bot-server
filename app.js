f5865e53f412a12ab67c8e75af45eb5201db964c
Michael Aro <arom@us.ibm.com>
2018-01-01 14:24:50 -0600

    initial commit

--- app.js
+++ app.js
@@ -872,7 +872,7 @@ app.post('/ai', (req, res) => {
   if (req.body.result.action === 'weather') {
     console.log('*** weather ***');
     let city = req.body.result.parameters['geo-city'];
-    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+WEATHER_API_KEY+'&q='+city;
+    let restUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID='+OPENWEATHER_API_KEY+'&q='+city;
 
     request.get(restUrl, (err, response, body) => {
       if (!err && response.statusCode == 200) {
